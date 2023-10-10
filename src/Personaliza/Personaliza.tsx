bc575c6116800d554d5b5532e0dc5782396fea06
agossacu <agostinaacu28@gmail.com>
2023-10-10 20:08:57 -0300

    probandoe

--- src/Personaliza/Personaliza.tsx
+++ src/Personaliza/Personaliza.tsx
@@ -1,77 +1,168 @@
 import { RadioButtonChecked, RadioButtonUnchecked } from "@mui/icons-material";
-import { Box, Button, Container, List, ListItem, ListItemIcon, ListItemText, Modal, Typography, useMediaQuery, useTheme } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Modal,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import React from "react";
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  p: 4,
+};
 
-const Personaliza = ({ subjects }: { subjects: (string | boolean)[][][]; }) => {
+const Personaliza = ({ subjects }: { subjects: (string | boolean)[][][] }) => {
+  const [open, setOpen] = React.useState(false);
+  const [idMateria, setIdMateria] = React.useState(0);
+  const setHandleOpen = (id: number) => {
+    const handleOpen = () => {
+      setIdMateria(id);
+      setOpen(true);
+    };
+    return handleOpen;
+  };
+  const handleClose = () => setOpen(false);
 
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+  const theme = useTheme();
 
-    const theme = useTheme();
-    const md = useMediaQuery("min-width(900px)");
+  return (
+    <Box>
+      <Container>
+        <Box
+          justifyItems={"center"}
+          display={"grid"}
+          gridTemplateColumns={{ xs: "1fr", md: "1fr 1fr" }}
+          gridTemplateRows={{ xs: "80px", md: "130px" }}
+          gridTemplateAreas={{
+            xs: "'title' 'subjects' 'footer' ",
+            md: "'title title' 'subjects subjects'",
+          }}
+          height={{ md: "92vh", xs: "93vh" }}
+        >
+          <Box
+            height={"75px"}
+            paddingTop={{ xs: "10px", md: "30px" }}
+            gridArea={"title"}
+            alignSelf={"start"}
+            textAlign={"center"}
+          >
+            <Typography fontSize={{ xs: "40px", md: "55px" }} fontWeight="bold">
+              Personaliza
+            </Typography>
+          </Box>
 
-    return (
-        <Box>
-            <Container>
-                <Box justifyItems={"center"} display={"grid"} gridTemplateColumns={{ xs: "1fr", md: "1fr 1fr", }} gridTemplateRows={{ xs: "80px", md: "130px" }} gridTemplateAreas={{ xs: "'title' 'subjects' 'footer' ", md: "'title title' 'subjects subjects'" }} height={{ md: "92vh", xs: "93vh" }} >
-
-                    <Box height={"75px"} paddingTop={{ xs: "10px", md: "30px" }} gridArea={"title"} alignSelf={"start"} textAlign={"center"}>
-                        <Typography fontSize={{ xs: "40px", md: "55px" }} fontWeight="bold">Personaliza</Typography>
-                    </Box>
-
-                    <Box gridArea={"subjects"} display={"grid"} columnGap={{ md: "100px" }} rowGap={{ xs: "5px", md: "30px" }} justifyItems={"start"} alignContent={{ xs: "end", md: "center" }} gridTemplateColumns={{ md: "1fr 1fr 1fr", xs: "1fr" }} gridTemplateRows="auto" gridAutoColumns={"1fr"} gridAutoRows={"auto"}>
-                        {
-                            subjects.map(elem => (
-                                <List sx={{ width: "100%", display: "flex", flexDirection: "column", paddingBottom: "15px", borderBottom: "solid 1px", borderColor: theme.palette.primary.main }}>
-                                    {elem.map((item) => (
-                                        <ListItem sx={{ padding: "2px" }}>
-                                            <ListItemIcon sx={{ justifyContent: "center" }}>
-                                                {item[1] == true ? <RadioButtonChecked sx={{ fontSize: { md: "30px", xs: "20px" } }} /> : ""}
-                                                {item[2] == true ? <RadioButtonChecked sx={{ color: theme.palette.primary.main, fontSize: { md: "30px", xs: "20px" } }} /> : ""}
-                                                {item[1] == false && item[2] == false ? <RadioButtonUnchecked sx={{ fontSize: { md: "30px", xs: "20px" } }} /> : ""}
-                                            </ListItemIcon>
-                                            <ListItemText>
-                                                <Button onClick={handleOpen} sx={{padding:{xs:"1px",md:"4px"},display:"block",textAlign:"start",minWidth:"0px",lineHeight:"normal",textTransform:"none",color:"black",textDecoration:"none",paddingLeft:{xs:"0px",md:"10px"},fontSize:{xs:"15px",md:"20px"}}}>{item[0]}</Button>
-                                            </ListItemText>
-                                        </ListItem>
-                                    ))}
-                                </List>
-                            ))
-                        }
-                    </Box>
-
-
-
-
-                    <div>
-                        <Modal open={open} onClose={handleClose}>
-                            <Box>
-                                <Typography id="modal-modal-title" variant="h6" component="h2">
-                                    modal
-                                </Typography>
-                                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                                    modal
-                                </Typography>
-                            </Box>
-                        </Modal>
-                    </div>
-
-
-
-
-
-
-
-                    <Box gridArea={"footer"} height={"40px"} width={"100%"}>
-
-                    </Box>
-
-                </Box>
-            </Container>
+          <Box
+            gridArea={"subjects"}
+            display={"grid"}
+            columnGap={{ md: "100px" }}
+            rowGap={{ xs: "5px", md: "30px" }}
+            justifyItems={"start"}
+            alignContent={{ xs: "end", md: "center" }}
+            gridTemplateColumns={{ md: "1fr 1fr 1fr", xs: "1fr" }}
+            gridTemplateRows="auto"
+            gridAutoColumns={"1fr"}
+            gridAutoRows={"auto"}
+          >
+            {subjects.map((elem) => (
+              <List
+                sx={{
+                  width: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                  paddingBottom: "15px",
+                  borderBottom: "solid 1px",
+                  borderColor: theme.palette.primary.main,
+                }}
+              >
+                {elem.map((item, index) => (
+                  <ListItem key={index} sx={{ padding: "2px" }}>
+                    <ListItemIcon sx={{ justifyContent: "center" }}>
+                      {item[1] == true ? (
+                        <RadioButtonChecked
+                          sx={{ fontSize: { md: "30px", xs: "20px" } }}
+                        />
+                      ) : (
+                        ""
+                      )}
+                      {item[2] == true ? (
+                        <RadioButtonChecked
+                          sx={{
+                            color: theme.palette.primary.main,
+                            fontSize: { md: "30px", xs: "20px" },
+                          }}
+                        />
+                      ) : (
+                        ""
+                      )}
+                      {item[1] == false && item[2] == false ? (
+                        <RadioButtonUnchecked
+                          sx={{ fontSize: { md: "30px", xs: "20px" } }}
+                        />
+                      ) : (
+                        ""
+                      )}
+                    </ListItemIcon>
+                    <ListItemText>
+                      <Button
+                        onClick={setHandleOpen(index)}
+                        sx={{
+                          padding: { xs: "1px", md: "4px" },
+                          display: "block",
+                          textAlign: "start",
+                          minWidth: "0px",
+                          lineHeight: "normal",
+                          textTransform: "none",
+                          color: "black",
+                          textDecoration: "none",
+                          paddingLeft: { xs: "0px", md: "10px" },
+                          fontSize: { xs: "15px", md: "20px" },
+                        }}
+                      >
+                        {item[0]}
+                      </Button>
+                    </ListItemText>
+                    <Modal
+                      open={open}
+                      onClose={handleClose}
+                      aria-labelledby="modal-modal-title"
+                      aria-describedby="modal-modal-description"
+                    >
+                      <Box sx={style}>
+                        <Typography
+                          id="modal-modal-title"
+                          variant="h6"
+                          component="h2"
+                        >
+                          {elem[idMateria]}
+                        </Typography>
+                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                          {elem[idMateria]}
+                        </Typography>
+                      </Box>
+                    </Modal>
+                  </ListItem>
+                ))}
+              </List>
+            ))}
+          </Box>
+          <Box gridArea={"footer"} height={"40px"} width={"100%"}></Box>
         </Box>
-    );
+      </Container>
+    </Box>
+  );
 };
 
 export default Personaliza;
--- src/database/database.tsx
+++ src/database/database.tsx
@@ -1,9 +1,11 @@
 export const carreras = [
   {
-    icono: "https://images.vexels.com/media/users/3/136559/isolated/preview/624dd0a951a1e8a118215b1b24a0da59-logotipo-de-farmacia.png",
+    icono:
+      "https://images.vexels.com/media/users/3/136559/isolated/preview/624dd0a951a1e8a118215b1b24a0da59-logotipo-de-farmacia.png",
     nombre: "Farmacia",
     href: "/plandeestudio/0",
-    descripcion: "Profesionales de la salud expertos en medicamentos destinados a la curación, alivio, prevención y diagnóstico de enfermedades.",
+    descripcion:
+      "Profesionales de la salud expertos en medicamentos destinados a la curación, alivio, prevención y diagnóstico de enfermedades.",
     materias: [
       [
         ["Quimica general e inorgánica", true, false],
@@ -11,7 +13,7 @@ export const carreras = [
         ["Quimica", false, false],
         ["bioquimica", false, true],
         ["farmacologia", false, true],
-        ["bioquimica", false, true],        
+        ["bioquimica", false, true],
         ["Fisica", false, false],
         ["Microbiologia", false, false],
       ],
@@ -38,63 +40,68 @@ export const carreras = [
         ["Fisica", false, false],
         ["Microbiologia", false, false],
       ],
-    ]
+    ],
   },
   {
     icono: "https://cdn-icons-png.flaticon.com/512/6166/6166904.png",
     nombre: "Bioquímica",
     href: "/plandeestudio/1",
-    descripcion: "Son profesionales de la salud expertos en el diseño, realización e interpretación de análisis clínicos",
+    descripcion:
+      "Son profesionales de la salud expertos en el diseño, realización e interpretación de análisis clínicos",
     materias: [
       [
         ["Bioquimica", true, false],
         ["fisiologia", false, true],
         ["radiologia", true, false],
         ["algebra", true, false],
-        ["filosofia", false, false]
-      ], 
+        ["filosofia", false, false],
+      ],
       [
         ["quimica", false, false],
-        ["fisica", false, false]
-      ]
-    ]
+        ["fisica", false, false],
+      ],
+    ],
   },
   {
-    icono: "https://www.gba.gob.ar/sites/default/files/desarrolloagrario/newsletter/images/irradiar_alimentos.png",
+    icono:
+      "https://www.gba.gob.ar/sites/default/files/desarrolloagrario/newsletter/images/irradiar_alimentos.png",
     nombre: "Licenciatura en Ciencias y Tecnología de Alimentos",
     href: "/plandeestudio/2",
-    descripcion: "Son profesionales competentes para realizar actividades relacionadas con la investigación de todo tipo de alimentos",
+    descripcion:
+      "Son profesionales competentes para realizar actividades relacionadas con la investigación de todo tipo de alimentos",
     materias: [
       [
         ["nutricion", false, true],
         ["fisiologia", false, true],
         ["cardiologia", false, true],
         ["biologia", true, false],
-        ["farmacologia", false, true]
-      ], 
+        ["farmacologia", false, true],
+      ],
       [
-        ["agronomia", false, false], 
-        ["economia", false, false]
-      ]
-    ]
+        ["agronomia", false, false],
+        ["economia", false, false],
+      ],
+    ],
   },
   {
-    icono: "https://www.intecnus.org.ar/wp-content/uploads/2018/10/Medicina-Nuclear.png",
+    icono:
+      "https://www.intecnus.org.ar/wp-content/uploads/2018/10/Medicina-Nuclear.png",
     nombre: "Tecnicatura Universitaria en Medicina Nuclear",
     href: "/plandeestudio/3",
-    descripcion: "Son profesionales de la salud competentes para realizar, bajo supervisión del médico, procedimientos diagnósticos y terapéuticos en pacientes mediante la utilización de radiofármacos",
+    descripcion:
+      "Son profesionales de la salud competentes para realizar, bajo supervisión del médico, procedimientos diagnósticos y terapéuticos en pacientes mediante la utilización de radiofármacos",
     materias: [
       [
         ["energia nuclear", false, true],
         ["radiologia", true, false],
         ["algebra", true, false],
         ["matematica", true, false],
-        ["analisis matematico", false, true]
-      ], 
+        ["analisis matematico", false, true],
+      ],
       [
         ["filosofia", false, false],
-        ["procesos productivos", false, false]
-      ]
-    ]
+        ["procesos productivos", false, false],
+      ],
+    ],
   },
 ];
