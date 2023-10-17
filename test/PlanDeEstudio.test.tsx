dc17941913ab1fdac825298bc589213619d998b6
Joaquin Blanco <blancojoaquin77@gmail.com>
2023-10-17 13:56:08 -0300

    se supero el test propuesto para planes

--- test/PlanDeEstudio.test.tsx
+++ test/PlanDeEstudio.test.tsx
@@ -1,20 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import CarreraComponent from "@/src/Carrera/Carrera";
+import CCarrera from "@/src/Carrera/Carrera";
 import { farmaciaMock } from "@/src/database/mocks";
 
 describe("Dashboard sobre el estado actual del plan de estudios de una carrera", () => {
-  test("Deberia mostrar como titulo 'Farmacia' siendo Farmacia el plan seleccionado", () => {
-    render(<CarreraComponent carrera={farmaciaMock} />);
+  test("Deberia mostrar como titulo 'Farmacia' siendo Farmacia el plan seleccionado", async () => {
+    render(<CCarrera carrera={farmaciaMock} />);
     const nombre = farmaciaMock.nombre;
-    const titulo = screen.findByText(nombre);
+    const titulo = await screen.findByText(nombre);
     expect(titulo).toBeVisible();
   });
 
-  test("Deberia mostrar que un porcentaje de las materias estan aprobadas", () => {
-    render(<CarreraComponent carrera={farmaciaMock} />);
+  test("Deberia mostrar que un porcentaje de las materias estan aprobadas", async () => {
+    render(<CCarrera carrera={farmaciaMock} />);
     const porcentaje = farmaciaMock.porcentajeDeMateriasAprobadas();
-    const porcentajeComponent = screen.findByText(String(porcentaje));
-    expect(porcentajeComponent).toBeVisible();
+    const cporcentajes = await screen.findAllByText(`${porcentaje}%`);
+    cporcentajes.forEach((cporcentaje) => expect(cporcentaje).toBeVisible());
   });
 });
