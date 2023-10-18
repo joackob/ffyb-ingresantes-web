d364b55ab6a2fb17c9cb2bd9180b86ea4c55b39d
mecro <66536796+HackML@users.noreply.github.com>
2023-10-18 00:31:04 +0000

    Update Espacio.tsx
    arreglando el boton

--- src/espacios/Espacio.tsx
+++ src/espacios/Espacio.tsx
@@ -1,5 +1,16 @@
+//https://mui.com/material-ui/ materiales pantillas etc etc
+
 import "./Espacio.module.css";
-import { Box, Container, useMediaQuery, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  useMediaQuery,
+  Typography,
+  Select,
+  FormControl,
+  MenuItem,
+  InputLabel,
+} from "@mui/material";
 const Espacio = () => {
   const md = useMediaQuery("min-width(900px)");
 
@@ -7,14 +18,21 @@ const Espacio = () => {
     <Box>
       <div id="contenedor-general">
         <div id="paneles-superiores">
-          <div id="contenedor-elegir-videos">
-            <form action="" id="elegir-videos">
-              <select name="menu-video" id="menu-video">
-                <option value="video-general">Video General</option>
-                <option value="primer-piso">Primer Piso</option>
-                <option value="segundo-piso">Segundo Piso</option>
-              </select>
-            </form>
+          <div>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">
+                Video General
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                label="Video General"
+              >
+                <MenuItem value="video-general">Video General</MenuItem>
+                <MenuItem value="primer-piso">Primer Piso</MenuItem>
+                <MenuItem value="segundo-piso">Segundo Piso</MenuItem>
+              </Select>
+            </FormControl>
           </div>
         </div>
         <div id="videos">
