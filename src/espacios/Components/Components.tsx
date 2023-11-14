7a88f80e5cc80cb7c9aa5f4f5b0f27cbd7707f27
mecro <66536796+lukasbentivenga@users.noreply.github.com>
2023-11-14 14:43:15 +0000

    HAY RESPONSIVEEEEEEEEEEEEEE

--- src/espacios/Components/Components.tsx
+++ src/espacios/Components/Components.tsx
@@ -1,12 +1,30 @@
-import * as React from "react";
-import useMediaQuery from "@mui/material/useMediaQuery";
+import { useEffect, useRef } from 'react';
 
-const videoResponsive = () => {
-  if (useMediaQuery("(min-width:900px)")) {
-    return <video src="https://sharedby.blomp.com/kzv6mK" />;
-  } else {
-    return <video src="https://www.youtube.com/watch?v=Jq57zqfqF6s" />;
-  }
-};
+const TuComponente = () => {
+  const refVideo = useRef(null);
 
-export default videoResponsive;
+  useEffect(() => {
+    const handleResize = () => {
+      const video = refVideo.current;
+
+      if (video) {
+        const newVideoSource = window.innerWidth >= 1024
+          ? 'https://sharedby.blomp.com/kzv6mK'
+          : 'https://sharedby.blomp.com/otrovideo';
+
+        video.src = newVideoSource;
+        video.load(); // Recarga el video con el nuevo origen
+      }
+    };
+
+    // Establece el origen del video al cargar la página
+    handleResize();
+
+    // Agrega un listener para cambiar el origen del video al cambiar el tamaño de la ventana
+    window.addEventListener('resize', handleResize);
+
+    // Limpia el listener cuando el componente se desmonta
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  export default TuComponente;
--- src/espacios/Espacio.tsx
+++ src/espacios/Espacio.tsx
@@ -18,10 +18,29 @@ import {
 } from "@mui/material";
 import { useState } from "react";
 import { useRef } from "react";
+import { useEffect } from "react";
 import Modal from "@mui/material/Modal";
 
 const Espacio = () => {
   const md = useMediaQuery("min-width(900px)");
+  const telefono = useMediaQuery("max-width(900px)");
+
+  //Video Responsive
+  const videoSources = {
+    desktop: "https://sharedby.blomp.com/qTphr8",
+    mobile: "https://sharedby.blomp.com/kzv6mK",
+  };
+
+  const [videoSource, setVideoSource] = useState(videoSources.desktop);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    if (md) {
+      setVideoSource(videoSources.desktop);
+    } else {
+      setVideoSource(videoSources.mobile);
+    }
+  }, [md, videoSources]);
 
   //Modal
   const [open, setOpen] = React.useState(false);
@@ -54,95 +73,97 @@ const Espacio = () => {
   return (
     <Box id="contenedor-supremo" sx={{ marginTop: "30px" }}>
       <Box id="contenedor-general">
-        <Box
-          id="paneles-superiores"
-          display={"flex"}
-          justifyContent={"center"}
-          flexDirection={"row"}
-        >
-          <Container id="elegir-video" sx={{ marginBottom: "20px" }}>
-            <FormControl fullWidth>
-              <InputLabel
-                id="demo-simple-select-label"
-                sx={{ color: "#3728b7" }}
-              >
-                {" "}
-                Select Video
-              </InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                defaultValue={"video-general"}
-                sx={{ color: "#3728b7" }}
-              >
-                <MenuItem value="video-general">Video General</MenuItem>
-                <MenuItem value="primer-piso">Primer Piso</MenuItem>
-                <MenuItem value="segundo-piso">Segundo Piso</MenuItem>
-              </Select>
-            </FormControl>
-          </Container>
+        <Box id="layout">
+          <Box
+            id="paneles-superiores"
+            display={"flex"}
+            justifyContent={"center"}
+            flexDirection={"row"}
+          >
+            <Container id="elegir-video" sx={{ marginBottom: "20px" }}>
+              <FormControl fullWidth>
+                <InputLabel
+                  id="demo-simple-select-label"
+                  sx={{ color: "#3728b7" }}
+                >
+                  {" "}
+                  Select Video
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  defaultValue={"video-general"}
+                  sx={{ color: "#3728b7" }}
+                >
+                  <MenuItem value="video-general">Video General</MenuItem>
+                  <MenuItem value="primer-piso">Primer Piso</MenuItem>
+                  <MenuItem value="segundo-piso">Segundo Piso</MenuItem>
+                </Select>
+              </FormControl>
+            </Container>
 
-          <Container id="planos" sx={{ marginBottom: "20px" }}>
-            <Button
-              onClick={handleOpen}
-              variant="outlined"
-              size="large"
-              fullWidth
+            <Container id="planos" sx={{ marginBottom: "20px" }}>
+              <Button
+                onClick={handleOpen}
+                variant="outlined"
+                size="large"
+                fullWidth
+                sx={{
+                  height: "100%",
+                  color: "#3728b7",
+                  borderColor: "#3728b7",
+                }}
+              >
+                Ver Plano
+              </Button>
+              <Modal
+                id="modal"
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box sx={style}>
+                  <img src="https://i.pinimg.com/236x/9b/53/c7/9b53c7056b72da324df08aed0d7be51f.jpg" />
+                </Box>
+              </Modal>
+            </Container>
+          </Box>
+
+          <Container id="videos" sx={{ position: "relative" }}>
+            <video id="video-space" ref={refVideo} width="100%">
+              <source src={videoSource} />
+            </video>
+            <Container
               sx={{
-                height: "100%",
-                color: "#3728b7",
-                borderColor: "#3728b7",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                zIndex: 1,
               }}
             >
-              Ver Plano
-            </Button>
-            <Modal
-              id="modal"
-              open={open}
-              onClose={handleClose}
-              aria-labelledby="modal-modal-title"
-              aria-describedby="modal-modal-description"
-            >
-              <Box sx={style}>
-                <img src="https://i.pinimg.com/236x/9b/53/c7/9b53c7056b72da324df08aed0d7be51f.jpg" />
-              </Box>
-            </Modal>
+              {!reproduciendo && (
+                <IconButton onClick={handlePressPlay}>
+                  <PlayCircleIcon
+                    sx={{ fontSize: "8rem", color: "#3728b7", opacity: "80%" }}
+                  />
+                </IconButton>
+              )}
+
+              {reproduciendo && (
+                <IconButton onClick={handlePressPause}>
+                  <PauseCircleIcon
+                    sx={{ fontSize: "8rem", color: "#3728b7", opacity: "80%" }}
+                  />
+                </IconButton>
+              )}
+            </Container>
           </Container>
         </Box>
-
-        <Container id="videos" sx={{ position: "relative" }}>
-          <video
-            id="video-space"
-            src="https://sharedby.blomp.com/kzv6mK"
-            ref={refVideo}
-            width="100%"
-          />
-        </Container>
-        <Container
-          sx={{
-            position: "absolute",
-            top: "75%",
-            display: "flex",
-            justifyContent: "center",
-            zIndex: 1,
-          }}
-        >
-          {!reproduciendo && (
-            <IconButton onClick={handlePressPlay}>
-              <PlayCircleIcon
-                sx={{ fontSize: "8rem", color: "#3728b7", opacity: "80%" }}
-              />
-            </IconButton>
-          )}
-
-          {reproduciendo && (
-            <IconButton onClick={handlePressPause}>
-              <PauseCircleIcon
-                sx={{ fontSize: "8rem", color: "#3728b7", opacity: "80%" }}
-              />
-            </IconButton>
-          )}
-        </Container>
       </Box>
     </Box>
   );
