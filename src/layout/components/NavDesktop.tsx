a43bc7ab9fd05149039296467c426cc1416897f5
facundo caballi <facundo caballi>
2024-06-07 17:21:25 -0300

    le agregue un hover y un action al header

--- src/layout/components/NavDesktop.tsx
+++ src/layout/components/NavDesktop.tsx
@@ -1,4 +1,4 @@
-import { Breadcrumbs, Container, Link, Stack, Typography} from "@mui/material";
+import { Breadcrumbs, Container, Link, Stack, Typography,useTheme } from "@mui/material";
 import { ReactNode } from "react";
 const NavDesktop = ({
   linksLeft,
@@ -6,18 +6,29 @@ const NavDesktop = ({
 }: {
   linksLeft: { label: string; href: string; icon: ReactNode }[];
   linksRight: { label: string; href: string; icon: ReactNode }[];
-}) => (
-  <Container sx={{height: "8vh",padding:"0px" }}>
+}) => {
+  const theme = useTheme();
+  return(
+  <Container sx={{ height: "8vh", padding: "0px" }}>
     <Stack
       height="100%"
       width="100%"
       alignItems="center"
       justifyContent="space-around"
       direction="row"
+
+
     >
       <Breadcrumbs separator={" "}>
         <Stack alignItems="center" direction="row" spacing={1}>
-          <Link href="/" underline="none" color="inherit" fontWeight="bold">
+          <Link href="/" underline="none" color="inherit" fontWeight="bold" sx={{
+            "&:hover": {
+              color:  theme.palette.primary.main,
+            },
+            '&:active': {
+              color:  theme.palette.primary.dark, // Cambia el color cuando se hace clic en el enlace
+            },
+          }}>
             <Typography marginRight={"1.5vw"} variant="inherit">Home</Typography>
           </Link>
         </Stack>
@@ -27,8 +38,17 @@ const NavDesktop = ({
             <Link
               href={item.href}
               underline="none"
-              color="inherit"
+              color="bkack"
               fontWeight="bold"
+              sx={{
+                "&:hover": {
+                  color: theme.palette.primary.main,
+                },
+
+                '&:active': {
+                  color:  theme.palette.primary.dark, // Cambia el color cuando se hace clic en el enlace
+                },
+              }}
             >
               <Typography marginRight={"1.5vw"} variant="inherit">{item.label}</Typography>
             </Link>
@@ -38,7 +58,7 @@ const NavDesktop = ({
       <Breadcrumbs separator={" "}>
         {linksRight.map((item, index) => (
           <Stack key={index} spacing={1} alignItems="center" direction="row">
-            <Link href={item.href} display={"flex"} justifyContent={"space-between"} color={"#3728b7"} border={"solid 2.5px"}borderRadius={"10px"} borderColor={"#3728b7"} padding={"10px 20px"} underline="none" fontWeight={"bold"}>
+            <Link href={item.href} display={"flex"} justifyContent={"space-between"} color={"#3728b7"} border={"solid 2.5px"} borderRadius={"10px"} borderColor={"#3728b7"} padding={"10px 20px"} underline="none" fontWeight={"bold"}>
               {item.icon}
               <Typography marginLeft={"10px"} variant="inherit">{item.label}</Typography>
             </Link>
@@ -47,6 +67,7 @@ const NavDesktop = ({
       </Breadcrumbs>
     </Stack>
   </Container>
-);
+  )
+};
 
 export default NavDesktop;
