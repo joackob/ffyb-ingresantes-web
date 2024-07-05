fbae2875dc555716d5fcb0ac0e6c4fc701710abb
Nikolakito <nikolakitoferreiros@gmail.com>
2024-07-05 17:47:42 -0300

    arregle errores de mensaje principal, y apenas empece con tarjeta home.

--- src/home/components/MensajePrincipal.tsx
+++ src/home/components/MensajePrincipal.tsx
@@ -2,38 +2,41 @@ import {
     Box,
     Container,
     Typography,
-    useTheme,
   } from "@mui/material";
 
   const MensajePrincipal = () => {
-    const theme = useTheme();
-    const title = "Bienvenidos al Sistema de Tutorías de Farmacia y bioquímica.\n ";
+    const promo = "Bienvenidos al Sistema de Tutorías de Farmacia y bioquímica.\n ";
   return(
   <Box 
-  width={{md: "1024px", xs: "430px"}}  
+  width={"100%"}  
   textAlign={"center"}
-  fontWeight="bold"
+  fontWeight="800"
   height={"448px"}
   display="flex" 
   flexDirection="column" 
   justifyContent="space-between" 
   alignItems="center"
   color={"#FFFFFF"}
-  padding={"10px"}
+  fontFamily={"Montserrat"}
+  
+  
   >
   <Container >
             <Typography
               variant={"h1"}
-              
+              fontSize={"48px"}
               display={{ md: "block", xs: "none" }}
+              padding={"10px"}
             >
-              {title}
+              {promo}
             </Typography>
             <Typography
               variant={"h3"}
+              fontSize={"40px"}
               display={{ md: "none", xs: "block" }}
+              padding={"10px"}
             >
-              {title}
+              {promo}
             </Typography>
   </Container>
   </Box>
