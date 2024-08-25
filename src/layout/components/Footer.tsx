dd4029c480258aed4c588fdf8f02c89bd1a2556d
Joaquin Blanco <blancojoaquin77@gmail.com>
2024-08-25 11:26:22 -0300

    fix: el footer tiene los estilos adecuados

--- src/layout/components/Footer.tsx
+++ src/layout/components/Footer.tsx
@@ -1,28 +1,45 @@
 import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
 
+const informacion = [
+  "Facultad de farmacia y bioquimica - UBA",
+  "Junin 954 Ciudad Autonoma de Buenos Aires, Argentina",
+  "TE:52875000",
+];
+
 const Footer = () => {
   return (
     <>
-      <Box component={"footer"}></Box>
-      <Stack
-        direction={"column"}
-        textAlign={"center"}
-        sx={{ backgroundColor: "#011344" }}
+      <Box
+        component={"footer"}
+        py={"32px"}
+        style={{ backgroundColor: "#011344", color: "white" }}
       >
-        <Box sx={{ color: "white" }} margin={"10px"}>
-          <Typography variant="h5">
-            Facultad de farmacia y bioquimica - UBA
-          </Typography>
-          <Typography variant="h5">
-            Junin 954 Ciudad Autonoma de Buenos Aires, Argentina
-          </Typography>
-          <Typography variant="h5">TE:52875000</Typography>
-          <Typography variant="h5" sx={{ color: "#FF8700" }}>
+        <Stack direction={"column"} textAlign={"center"} spacing={"12px"}>
+          {informacion.map((info) => (
+            <Typography
+              key={info}
+              style={{
+                fontFamily: "Montserrat",
+                fontSize: "16px",
+                fontWeight: "800",
+              }}
+            >
+              {info}
+            </Typography>
+          ))}
+          <Typography
+            style={{
+              color: "#FF8700",
+              fontFamily: "Montserrat",
+              fontSize: "16px",
+              fontWeight: "800",
+            }}
+          >
             Hecho con 💕 por ETEC-UBA
           </Typography>
-        </Box>
-      </Stack>
+        </Stack>
+      </Box>
     </>
   );
 };
