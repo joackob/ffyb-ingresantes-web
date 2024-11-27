fb710d00cac2afaa96b9257a775c2845c05313bc
AymaraQC <qcaymara18@gmail.com>
2024-11-27 17:11:18 -0300

    Termine con los arreglos en la seccion del sistema de tutorias(Falta la aprobacion del profe)

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -4,12 +4,55 @@ import type { MDXComponents } from "mdx/types";
 export const customComponents: MDXComponents = {
   h2: ({ children }) => (
     <Typography
-      component={"h1"}
+      component={"h2"}
       sx={{
         color: "#011344",
         fontFamily: "Montserrat",
         fontWeight: "ExtraBold",
         fontSize: "16px",
+        marginTop: "50px", // Agrega margen superior
+        marginBottom: "20px", // Agrega margen inferior
+        paddingLeft: "50px", // Agrega relleno en el lado izquierdo
+        //paddingRight: "15px", // Agrega relleno en el lado derecho
+        backgroundColor: "#e7f5ec", // Cambia el color de fondo
+        
+      }}
+    >
+      {children}
+    </Typography>
+  ),
+  h3: ({ children }) => (
+    <Typography
+      component={"h3"}
+      sx={{
+        color: "#7C7C7C",
+        fontFamily: "Montserrat",
+        fontWeight: "ExtraBold",
+        fontSize: "16px",
+        marginTop: "50px", // Agrega margen superior
+        marginBottom: "20px", // Agrega margen inferior
+        paddingLeft: "50px", // Agrega relleno en el lado izquierdo
+        //paddingRight: "15px", // Agrega relleno en el lado derecho
+        
+      }}
+    >
+      {children}
+    </Typography>
+  ),
+
+  h5: ({ children }) => (
+    <Typography
+      component={"h5"}
+      sx={{
+        color: "#7C7C7C",
+        fontFamily: "Montserrat",
+        fontWeight: "ExtraBold",
+        fontSize: "16px",
+        marginTop: "50px", // Agrega margen superior
+        marginBottom: "20px", // Agrega margen inferior
+        paddingLeft: "50px", // Agrega relleno en el lado izquierdo
+        //paddingRight: "15px", // Agrega relleno en el lado derecho
+        
       }}
     >
       {children}
@@ -23,11 +66,20 @@ export const customComponents: MDXComponents = {
         fontFamily: "Montserrat",
         fontWeight: "Regular",
         fontSize: "16px",
+        marginTop: "20px", // Agrega margen superior
+        paddingLeft: "50px", // Agrega relleno en el lado izquierdo
+        paddingRight: "50px", // Agrega relleno en el lado derecho
+        //textAlign: "center", // Centra el texto
+        marginBottom: "50px",
+
       }}
     >
       {children}
     </Typography>
   ),
+
+//PREGUNTAR A FEDE SOBRE ESTO :    marginBottom: "20px", EN LA PARTE DE ABAJO
+  
 };
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
