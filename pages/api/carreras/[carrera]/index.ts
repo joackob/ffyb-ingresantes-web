062d057f49e9e223848a9ca34c7041c8cb26a85a
Jesica606 <jesicaaraibar17@gmail.com>
2024-12-05 19:17:50 -0300

    termine de corregir el switch case y cree una funcion para encontrar una carrera espesifica con sus cuatrimestres y contenidos

--- pages/api/carreras/[carrera]/index.ts
+++ pages/api/carreras/[carrera]/index.ts
@@ -1,38 +1,67 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { brindarInformacionPorDefectoDeLasCarreras } from "@/src/api/carreras/intentar-brindar-informacion-por-defecto-de-las-carreras";
+import path from "path";
+import fs from "fs/promises";
 
 const route = async (consulta: NextApiRequest, respuesta: NextApiResponse) => {
   const carrera = consulta.query.carrera;
+  const direccionDeLosDatosConInformacionSobreLasCarreras = path.join(
+    process.cwd(),
+    "src/api/carreras/data",
+    "carreras.json"
+  );
+  const informacionEnCrudoDeLasCarreras = await fs.readFile(
+    direccionDeLosDatosConInformacionSobreLasCarreras,
+    { encoding: "utf-8" }
+  );
+  const informacionDeLasCarreras = JSON.parse(informacionEnCrudoDeLasCarreras);
+
+  const carreras = informacionDeLasCarreras["carreras"];
+
+  const encontrarCarrera = (carreraABuscar: string) => {
+    return (carreras as Array<{ nombre: string }>).find(
+      (carrera) => carrera.nombre === carreraABuscar
+    );
+  };
+
+  console.log(carreras);
   switch (carrera) {
     case "farmacia": {
-      return respuesta.json("Farmacia");
+      return respuesta.json(encontrarCarrera("farmacia"));
       break;
     }
     case "bioquimica": {
-      return respuesta.json("Bioquimica");
+      return respuesta.json(encontrarCarrera("Bioquimica"));
       break;
     }
     case "tecnico-universitario-en-medicina-nuclear": {
-      return respuesta.json("Técnico Universitario en Medicina Nuclear");
+      return respuesta.json(
+        encontrarCarrera("Técnico Universitario en Medicina Nuclear")
+      );
       break;
     }
     case "tecnico-universitario-en-optica-y-contactologia": {
-      return respuesta.json("Técnico Universitario en Óptica y Contactología");
+      return respuesta.json(
+        encontrarCarrera("Técnico Universitario en Óptica y Contactología")
+      );
       break;
     }
     case "licenciatura-en-ciencia-y-tecnologia-de-alimentos": {
       return respuesta.json(
-        "Licenciatura en Ciencia y Tecnología de Alimentos"
+        encontrarCarrera("Licenciatura en Ciencia y Tecnología de Alimentos")
       );
       break;
     }
     case "tecnicatura-universitaria-en-gestion-integral-de-bioterio": {
       return respuesta.json(
-        "Tecnicatura Universitaria en Gestión Integral de Bioterio"
+        encontrarCarrera(
+          "Tecnicatura Universitaria en Gestión Integral de Bioterio"
+        )
       );
       break;
     }
     default: {
-      return respuesta.status(404).json({ message: `carrera invalida` });
+      return respuesta.status(404).json({ message: `Carrera invalida` });
       break;
     }
   }
