57defaa7381450cfafa31d835c150d5370c0cf21
daiAPZ <azdelugano001mela@gmail.com>
2024-12-10 20:07:51 -0300

    agregue videos de ffyb con descripcion

--- src/espacios-de-la-facultad/videos/index.ts
+++ src/espacios-de-la-facultad/videos/index.ts
@@ -9,7 +9,7 @@ export const videos = [
 
   {
     id: "2",
-    titulo: "UBA | Facultad de Farmacia y Bioquimica | Aulas/Salones de clase",
+    titulo: "UBA | Facultad de Farmacia y Bioquimica | Aula Magna",
     descripcion:
       "Lorem ipsum dolor sit amet consectetur adipiscing elit sociosqu odio, cubilia faucibus gravida eros volutpat vulputate quam venenatis platea tempor, in ultricies imperdiet magna morbi nascetur dignissim penatibus. Sed magnis suspendisse penatibus senectus lacus montes tristique dignissim, imperdiet justo tincidunt ut dictum euismod massa rutrum elementum, aptent donec fermentum curae netus purus cras. Quam habitant ac ullamcorper pellentesque eleifend dictumst nam dictum interdum cubilia, venenatis himenaeos dis bibendum convallis diam donec cras nisl commodo, et taciti eget natoque torquent nullam justo at class.",
     url: "https://www.youtube.com/embed/tTmyiM8ziQk?feature=share",
@@ -25,9 +25,31 @@ export const videos = [
 
   {
     id: "4",
-    titulo: "UBA | Facultad de Farmacia y Bioquimica | Musio de Farmacia",
+    titulo: "UBA | Facultad de Farmacia y Bioquimica | Museo de Farmacia",
     descripcion:
       "Lorem ipsum dolor sit amet consectetur adipiscing elit sociosqu odio, cubilia faucibus gravida eros volutpat vulputate quam venenatis platea tempor, in ultricies imperdiet magna morbi nascetur dignissim penatibus. Sed magnis suspendisse penatibus senectus lacus montes tristique dignissim, imperdiet justo tincidunt ut dictum euismod massa rutrum elementum, aptent donec fermentum curae netus purus cras. Quam habitant ac ullamcorper pellentesque eleifend dictumst nam dictum interdum cubilia, venenatis himenaeos dis bibendum convallis diam donec cras nisl commodo, et taciti eget natoque torquent nullam justo at class.",
     url: "https://www.youtube.com/embed/7veGwrpXOXk?feature=share",
   },
+  {
+    id: "5",
+    titulo: "UBA | Facultad de Farmacia y Bioquimica | Centro de Estudiantes ",
+    descripcion:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit sociosqu odio, cubilia faucibus gravida eros volutpat vulputate quam venenatis platea tempor, in ultricies imperdiet magna morbi nascetur dignissim penatibus. Sed magnis suspendisse penatibus senectus lacus montes tristique dignissim, imperdiet justo tincidunt ut dictum euismod massa rutrum elementum, aptent donec fermentum curae netus purus cras. Quam habitant ac ullamcorper pellentesque eleifend dictumst nam dictum interdum cubilia, venenatis himenaeos dis bibendum convallis diam donec cras nisl commodo, et taciti eget natoque torquent nullam justo at class.",
+    url: "https://www.youtube.com/embed/7veGwrpXOXk?feature=share",
+  },
+  {
+    id: "6",
+    titulo: "UBA | Facultad de Farmacia y Bioquimica | PISO 1",
+    descripcion:
+      "El primer piso consta de: Tres Aulas (101, 102, 103) Tres laboratorios (Laboratorio de Microscopía, Laboratorio de T.P de Anatomía e Historia, Laboratorio Oca. Biología Vegetal) Cuatro Cátedras (cátedra de Oca. Biológica, Cátedra de Biología Celular y Molecular, Cátedra de Anatomía e Historia, Cátedra de Farmacobotánica) Dos Museos (Museo de Farmacobotánica, Museo de Farmacia) Una Biblioteca de Alumnos.",
+    url: "https://youtube.com/embed/6pkD4LZFJGU?feature=share",
+  },
+  {
+    id: "7",
+    titulo: "UBA | Facultad de Farmacia y Bioquimica | PISO 2",
+    descripcion:
+      "El segundo piso consta de: Dos Aulas (201, 202) Tres Laboratorios (Laboratorio de T.P de Oca.General e Inorgánica, Laboratorio de T.P de Fisicoquímica, Laboratorio de T.P de Bromatología y Nutrición) Cinco Cátedras (Cátedra de Farmacognosia - IQUIMEFA, Cátedra de Oca.General e Inorgánica, Cátedra de Fisicoquímica, Cátedra de Nutrición, Cátedra de Bromatología), Dos Museos (Museo de Farmacobotánica, Museo de Farmacia) Dos espacios, uno para la venta de apuntes y el otro para el Centro de Estudiantes.",
+    url: "https://www.youtube.com/embed/7rLI9hnwvJs?feature=share",
+  },
+
 ];
