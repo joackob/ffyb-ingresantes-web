d8853e1e1c3e976f9583e886e1e39706a42be3f5
PapelConPera <lionel.copajira@gmail.com>
2025-02-20 17:27:48 -0300

    agregue los integrantes y lideres del equipo

--- pages/integrantes/index.tsx
+++ pages/integrantes/index.tsx
@@ -1,22 +1,40 @@
 import { Box, Typography, Stack } from "@mui/material";
 
 const integrantes = [
-  { nombre: "felipe", email: "fe@gmail.com" },
-  { nombre: "felipe", email: "as@gmail.com" },
-  { nombre: "felipe", email: "as@gmail.com" },
-  { nombre: "felipe", email: "as@gmail.com" },
+  { nombre: "Nicolas Ferreiros", email: "nferreiros@etec.uba.ar" },
+  { nombre: "Agostina Acuña", email: "aacunamarichal@etec.uba.ar" },
+  { nombre: "Facundo Caballi", email: "facundocaballi@gmail.com" },
+  { nombre: "Tiago Nuñez", email: "tnunez17@gmail.com" },
+];
+
+const lideres = [
+  { nombre: "Joaquin Blanco", email: "jblanco@etec.uba.ar" },
+  { nombre: "Federico Villace", email: "fvillace@etec.uba.ar" },
 ];
 
 const Integrantes = () => {
   return (
     <Box p={4} textAlign="center">
-      <Typography variant="h4" fontWeight="bold" gutterBottom color="#FF8710">
+      {/* Sección de Integrantes */}
+      <Typography variant="h4" fontWeight="bold" gutterBottom color="#FF8700">
         Integrantes
       </Typography>
       <Stack spacing={2}>
         {integrantes.map((integrante, index) => (
-          <Typography key={index} variant="h7">
+          <Typography key={index} variant="h10">
             {integrante.nombre} - {integrante.email}
+          </Typography>
+        ))}
+      </Stack>
+
+      {/* Sección de Líderes de Equipo */}
+      <Typography variant="h4" fontWeight="bold" gutterBottom mt={4} color="#FF8700">
+        Líderes de Equipo
+      </Typography>
+      <Stack spacing={2}>
+        {lideres.map((lider, index) => (
+          <Typography key={index} variant="h10">
+            {lider.nombre} - {lider.email}
           </Typography>
         ))}
       </Stack>
